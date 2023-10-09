812f7bdeaf3f2d86596b7981cdd16aadf67d6c6b
sai aditya <saiaditya.yatthapu@transformco.com>
2023-10-09 14:36:41 +0530

    Removed Credentials

--- tests/pageObjects/authPage.ts
+++ tests/pageObjects/authPage.ts
@@ -12,8 +12,8 @@ export class AuthPage {
   }
 
   async auth() {
-    await this.ldapid.fill("syattha"); //Enter your ldap id
-    await this.password.fill("Homeservices#636"); // enter your password.
+    await this.ldapid.fill(""); //Enter your ldap id
+    await this.password.fill(""); // enter your password.
 
     await this.login.click();
   }
